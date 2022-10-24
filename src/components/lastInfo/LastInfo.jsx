7ce1fcc534d110d5cd6c6029fd62dcecc755661b
sla6ak <slabakxaker@gmail.com>
2022-10-25 01:29:53 +0300

    добавил кнопки удалить и редактировать

--- src/components/lastInfo/LastInfo.jsx
+++ src/components/lastInfo/LastInfo.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Container } from '../container/Container';
 import { BlokFooter, Reklama } from './LastInfo.styled';
+import { TelLink } from 'components/contacts/Contacts.styled';
+import { ReactComponent as Telegram } from '../../svg/icons8-telegram-app.svg';
 
 const LastInfo = () => {
     return (
         <BlokFooter>
             <Container>
-                <Reklama>LastInfo</Reklama>
+                <Reklama>
+                    <p>This site was developed</p>
+                    <TelLink sx={{ ml: '5px' }} target="blank" href="https://t.me/Tor_Ya">
+                        <p>Viktor</p>
+                        <Telegram height={'25px'} />
+                    </TelLink>
+                </Reklama>
             </Container>
         </BlokFooter>
     );
